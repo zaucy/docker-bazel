991ff48fead5bf708072538097a46ab21b514b70
zaucy <ezekiel@robotoppression.com>
2018-10-19 19:33:22 -0700

    Generate script + optimized gcc dockerfile

---
+++ generate.js
@@ -0,0 +1,69 @@
+const fs = require('fs-extra');
+const jsYaml = require('js-yaml');
+
+async function generate() {
+
+  const config = jsYaml.safeLoad(await fs.readFile('config.yml', 'utf-8'));
+
+  const services = {};
+
+  config.bazel.versions.unshift('');
+
+  const defaultBazelVersion = config.bazel.default;
+
+  for(let bazelVersion of config.bazel.versions) {
+
+    bazelVersion = bazelVersion.toString();
+
+    let bazelImgTitle = bazelVersion;
+
+    if(bazelImgTitle) {
+      bazelImgTitle += '-';
+    }
+
+    for(let {name, versions, default: defaultVersion} of config.compilers) {
+      const baseServiceName = ('bazel-' + bazelImgTitle + name).replace(/\./g, '-');
+      const baseTagName = name;
+      const imageName = 'zaucy/bazel';
+  
+      services[baseServiceName] = {
+        image: `${imageName}:${baseTagName}`,
+        build: {
+          context: name,
+          args: [
+            `BAZEL_VERSION=${bazelVersion || defaultBazelVersion}`,
+            `COMPILER_VERSION=${defaultVersion}`,
+          ],
+        },
+      }
+  
+      for(let version of versions) {
+        version = version.toString();
+
+        const serviceName = (baseServiceName + '-' + version).replace(/\./g, '-');
+        const tagName = baseTagName + '-' + version;
+  
+        services[serviceName] = {
+          image: `${imageName}:${tagName}`,
+          build: {
+            context: name,
+            args: [
+              `BAZEL_VERSION=${bazelVersion || defaultBazelVersion}`,
+              `COMPILER_VERSION=${version}`,
+            ],
+          },
+        };
+      }
+    }
+  }
+
+  await fs.writeFile(
+    `${__dirname}/docker-compose.yml`,
+    jsYaml.safeDump({version: '3', services}),
+  );
+}
+
+generate().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
